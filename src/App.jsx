b859b461bfa791efa24beaf750c4a6ac1b0a6686
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2025-03-31 17:57:24 +0000

    Lauren made these changes!

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,33 @@ import MovieProvider from './MovieContext'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
 import MovieGen from './pages/MovieGen'
+import logo from '/logo/logo.svg'
 
 function App() {
 
   return (
     <MovieProvider>
       <Router>
-
-        <div>
-          <nav>
+        <header>
+          <div className='Logohead'>
+            <img src={logo} alt="Cinemood Logo" className="logo" />
+            <h1 className='headertitle'>CINEMOOD</h1>
+          </div>
+          <nav className='Navigator'>
             <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/browse">Browse</Link>
-              </li>
-              <li>
-                <Link to="/generator">Movie Generator</Link>
-              </li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/browse">Browse</Link></li>
+              <li><Link to="/generator">Movie Generator</Link></li>
             </ul>
           </nav>
-
+        </header>
+        <div className='container'>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/browse" element={<Browse />} />
             <Route path="/generator" element={<MovieGen />} />
           </Routes>
         </div>
-
       </Router>
     </MovieProvider>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
-
 const Home = ({}) => {
 
 
     return (
-        <div>
-            Home Page Here
+        <div className="homepage">
+            <div>
+                <h1 className="welcome">Welcome to CineMood!</h1>
+                <p>Movies are more than just entertainment—they set the tone for our emotions, transport us to different worlds, and help us unwind after a long day. But with endless streaming options available, choosing what to watch can feel overwhelming. CineMood takes the guesswork out of the equation, offering hand-picked recommendations tailored to how you feel and even the weather around you.
+                Imagine a stormy evening, the rain tapping against your window—wouldn’t it be nice to cozy up with a thought-provoking drama or a nostalgic childhood favorite? Or maybe the sun is shining, and you’re in the mood for an uplifting comedy or an exciting adventure film? Whatever your mood, CineMood is here to guide you to the perfect watch.</p>
+            </div>
+            <div>
+                <h1 className="howitworks">How it works:</h1>
+                <p> 1. Tell Us How You Feel-Select your current mood, let us analyze the weather in your area or select a hobbie of yours. Get Tailored Recommendations</p> 
+                <p> 2. We curate a list of movies that match your vibe. Discover Something New</p> 
+                <p> 3. Find hidden gems, trending hits, or comforting favorites suited to your mood.</p>
+                <button>TRY IT NOW!</button>
+            </div>
+            <div className="footer">
+                <h1 className="emailhead">Join our Email list for all the new movie updates!</h1>
+                <form action="">
+                    <input type="Enter your email" placeholder="Enter your email"/>
+                    <button>Sign up!</button>
+                </form>
+            </div>
         </div>
     )
 }
