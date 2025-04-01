2a93314c8d35ad7e17f7da61bc1337afd1711270
laurenjacobonii <143565048+laurenjacobonii@users.noreply.github.com>
2025-04-01 18:24:02 +0000

    completed browse page template

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -8,8 +8,74 @@ const Browse = ({}) => {
     }, []);
 
     return (
-        <div>
-            Browse Page Here
+        <div className="homepage">
+            <div className="browsepage">
+                <h2 className="broswehead">Greatest Of All Time Movies</h2>
+                <div className="movieflex">
+                    <div className="moviebox">
+                        <p>flex 1</p>
+                    </div>
+                        <div className="moviebox">
+                            <p>flex 1b</p>
+                        </div>
+                    <div className="moviebox">
+                        <p>flex 2</p>
+                    </div>
+                    <div className="moviebox">
+                        <p>flex 2b</p>
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 3</p>
+                        <button class="arrow-button">→</button>
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 3b</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 4</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 4b</p>
+                        <button class="arrow-button">→</button>
+                    </div>
+                    <div className="moviebox">
+                        <p>flex 5</p>
+                    </div>
+                    <div className="moviebox">
+                        <p>flex 5b</p>
+                    </div>
+                    <div className="moviebox">
+                        <p>flex 6</p>
+                    </div>
+                    <div className="moviebox">
+                        <p>flex 6b</p>
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 7</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 7b</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 8</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                    <div className="movietitle">
+                        <p>flex 8b</p>
+                        <button class="arrow-button">→</button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
